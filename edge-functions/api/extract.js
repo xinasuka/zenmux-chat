91fb9de467c9062f8eb7421db57ca54605e85e2e
Charles Liu <xinasuka@126.com>
2026-09-02 17:20:53 +0800

    chore(v2.8.1): clean up legacy /api/search.js and /api/extract.js endpoint files

--- edge-functions/api/extract.js
+++
@@ -1,5 +0,0 @@
-// edge-functions/api/extract.js
-// Backward-compatibility wrapper forwarding to unified plugin gateway ./plugins/extract.js
-
-export { onRequestOptions, onRequestPost } from './plugins/extract.js';
-
--- edge-functions/api/search.js
+++
@@ -1,5 +0,0 @@
-// edge-functions/api/search.js
-// Backward-compatibility wrapper forwarding to unified plugin gateway ./plugins/search.js
-
-export { onRequestOptions, onRequestPost } from './plugins/search.js';
-
